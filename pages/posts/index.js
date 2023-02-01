acd98d64d3fe8d391831895df3a8f1c7a107d547
Klim <subbotin.klim@gmail.com>
2023-02-01 22:19:31 +0100

    add list of posts (with dummy examples)

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,26 +1,11 @@
-import Layout from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import {getSortedPostsData} from "../utils/posts";
-import Head from "next/head";
-
-export async function getStaticProps() {
-    const allPostsData = getSortedPostsData();
-    return {
-        props: {
-            allPostsData
-        }
-    }
-}
 export default function Home({ allPostsData }) {
     return (
         <Layout home>
             {/* Keep the existing code here */}
-            <Head>
-                <title>Paparrot</title>
-            </Head>
+
             {/* Add this <section> tag below the existing <section> tag */}
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-                <h2 className={utilStyles.headingLg}>My personal page</h2>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
                 <ul className={utilStyles.list}>
                     {allPostsData.map(({ id, date, title }) => (
                         <li className={utilStyles.listItem} key={id}>
